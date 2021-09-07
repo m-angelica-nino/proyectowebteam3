0c942a5e05aefa7ef901788d7a935820193257f5
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-07 11:58:43 -0500

    navbar
    Actualizar Navbar Juan Elias Vergara

--- Vista/principal.php
+++ Vista/principal.php
@@ -40,10 +40,11 @@
     <script src="js/script.js"></script>
   </head>
   <body>
-    <?php
+  <?php
       include_once('plantillanav.php');
     ?>
     <div class="wrap">
+
       <h1>RESERVA TU HOTEL</h1>
       <section class="">
         <p>
