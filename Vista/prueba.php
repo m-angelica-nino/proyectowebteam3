770eb1c8b536c36f1920d34f759b05c903f321ec
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-10 18:53:25 -0500

    Update prueba.php

--- Vista/prueba.php
+++ Vista/prueba.php
@@ -20,6 +20,7 @@
       rel="stylesheet"
     />
     <title>Document</title>
+
 </head>
 <body>
     <h1>texto</h1>
@@ -29,6 +30,7 @@
   <hr class="my-4">
   <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
   <p class="lead">
+      <h1></h1>
     <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
   </p>
 </div>
