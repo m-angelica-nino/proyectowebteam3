032415cf0dd17120e60434d60d26f83aefa144ef
JoseLuis1225 <90117170+JoseLuis1225@users.noreply.github.com>
2021-09-11 17:25:18 -0500

    Se crea vista registro
    Se crea la vista registro

--- Vista/registro.php
+++ Vista/registro.php
@@ -20,22 +20,15 @@
       rel="stylesheet"
     />
     <title>Document</title>
-
 </head>
 <body>
-    <h1>texto</h1>
-    <div class="jumbotron">
-  <h1 class="display-4 ejemplo">Hello, world!</h1>
-  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <hr class="my-4">
-  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-  <p class="lead">
+
+    <div class="jumbotron">       
     
-    <a class="btn btn-primary btn-lg" href="http://localhost/proyectowebteam3/Vista/principal.php" role="button">Learn more</a>
   </p>
 
-<center>
-<div class="card" style="width: 19rem;">
+
+<div class="card" style="width: 30rem;">
   <!-- <img src="..." class="card-img-top" alt="..."> -->
   <div class="card-body">
     <h5 class="card-title">Crea tu cuenta</h5>
@@ -44,28 +37,36 @@
 
     <form>
   <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+    <label for="exampleInputEmail1" class="form-label">Nombre</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">    
   </div>
   <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
+    <label for="exampleInputEmail1" class="form-label">Ciudad</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">    
   </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Telefono</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Direccion</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">    
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Correo Electronico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">    
+  </div>  
   <div class="mb-3">
     <label for="exampleInputPassword1" class="form-label">Password</label>
     <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
   <div class="mb-3 form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    <label class="form-check-label" for="exampleCheck1">Acepto los terminos y condiciones</label>
   </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" class="btn btn-primary">Crear Cuenta</button>
 </form>
-</center>
-  
 
-
-
+    
 </body>
 </html>
