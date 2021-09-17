4bb424d5ea27037eea9652e1cd4b39a387427088
m-angelica-nino <90117053+m-angelica-nino@users.noreply.github.com>
2021-09-16 20:42:17 -0500

    contador de visitas
    contador de visitas

--- sprint2/PaginaInicio/Vista/plantillanav.php
+++ sprint2/PaginaInicio/Vista/plantillanav.php
@@ -36,7 +36,7 @@
           <a class="dropdown-item" href="#"></span> Mis Reservas</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#"><span class="glyphiconglyphicon-user"></span> Salir</a>
-        </div>
+
         
       </li>
     <!---- Formato para busqueda
