a2684ad33ea37479ae91ed0bafb50bd74246f64f
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-19 01:58:57 -0500

    Logo

--- Vista/plantillanav.php
+++ Vista/plantillanav.php
@@ -1,5 +1,6 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!--   <li class="navbar-brand" href="#"><strong>DONDE ME QUEDO</strong></li> -->
   <li class="navbar-brand" href="#"><strong>DONDE ME QUEDO</strong></li>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -14,17 +15,6 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Acerca De</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Vista/registro.php">Registro</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../Vista/login.php">Login</a>
-      </li>
-      <!------- forrmato para deshabilitar un link
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-      --->
     </ul>
     <ul class="navbar-nav mr-auto displey-none">
     
@@ -42,11 +32,5 @@
 
         
       </li>
-    <!---- Formato para busqueda
-      <form class="form-inline my-2 my-lg-0">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    -->
   </div>
 </nav>
