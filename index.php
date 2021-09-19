87c12aa668e163d51f84f95bde77153074e4d852
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-18 23:16:13 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
 
-<head>
+<head> 
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" /> 
+    <title>Login</title> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" /> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
 
-    <link rel="stylesheet" href="css/estilos.css" />
-    <link rel="stylesheet" href="css/estyloslogin.css" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" />
+    <link rel="stylesheet" href="css/estilos.css" /> 
+    <link rel="stylesheet" href="css/estyloslogin.css" /> 
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" /> 
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> 
@@ -23,15 +23,15 @@
     <?php
     include_once 'Vista/plantillanavbaroffline.php'
     ?>
-    <div class="container">
-        <div class="d-flex justify-content-center h-100">
-            <div class="card">
-                <div class="card-header">
-                    <h3>BIENVENIDO</h3>
-                    <div class="d-flex justify-content-end social_icon">
-                        <span><i></i></span>
-                        <span><i></i></span>
-                        <span><i></i></span>
+    <div class="container"> 
+        <div class="d-flex justify-content-center h-100"> 
+            <div class="card"> 
+                <div class="card-header"> 
+                    <h3>BIENVENIDO</h3> 
+                    <div class="d-flex justify-content-end social_icon"> 
+                        <span><i></i></span> 
+                        <span><i></i></span> 
+                        <span><i></i></span> 
                     </div>
                 </div>
                 <div class="card-body">
