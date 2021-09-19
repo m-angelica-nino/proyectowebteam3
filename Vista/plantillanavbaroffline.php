1af106919b68b2483005c257aa61c1ddaf4e838c
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-18 21:40:59 -0500

    Update plantillanavbaroffline.php
    k

--- Vista/plantillanavbaroffline.php
+++ Vista/plantillanavbaroffline.php
@@ -11,6 +11,7 @@
         <a class="nav-link" href="../Vista/principal.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       
+      
       <li class="nav-item">
         <a class="nav-link" href="#">Acerca De</a>
       </li>
