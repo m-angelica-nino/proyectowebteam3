d7b0afcfc10a2b17b50d1648f68ef9ca2b47b928
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-19 10:17:39 -0500

    css
    css Juan Elias Vergara

--- Vista/perfil.php
+++ Vista/perfil.php
@@ -23,28 +23,29 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="profile-img">
-                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS52y5aInsxSm31CvHOFHWujqUx_wWTS9iM6s7BAm21oEN_RiGoog" alt="" />
+                        <img src="../src/imgUsuarios/Juan_Elias.jpeg" alt="" />
                         <div class="file btn btn-lg btn-primary">
-                            Change Photo
-                            <input type="file" name="file" />
+                            Cambiar Imagen
+                            <input type="file" name="img-perfil" />
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
+
                     <div class="profile-head">
                         <h5>
-                            Kshiti Ghelani
+                            Juan Elias Vergara
                         </h5>
                         <h6>
-                            Web Developer and Designer
+                            Sincelejo - Sucre
                         </h6>
-                        <p class="proile-rating">RANKINGS : <span>8/10</span></p>
+                        <p class="proile-rating">Reservas : <span>8</span></p>
                         <ul class="nav nav-tabs" id="myTab" role="tablist">
                             <li class="nav-item">
-                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">About</a>
+                                <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Datos de perfil</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Timeline</a>
+                                <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Datos Reservas</a>
                             </li>
                         </ul>
                     </div>
@@ -54,111 +55,90 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-4">
-                    <div class="profile-work">
-                        <p>WORK LINK</p>
-                        <a href="">Website Link</a><br />
-                        <a href="">Bootsnipp Profile</a><br />
-                        <a href="">Bootply Profile</a>
-                        <p>SKILLS</p>
-                        <a href="">Web Designer</a><br />
-                        <a href="">Web Developer</a><br />
-                        <a href="">WordPress</a><br />
-                        <a href="">WooCommerce</a><br />
-                        <a href="">PHP, .Net</a><br />
-                    </div>
-                </div>
-                <div class="col-md-8">
+                <div class="col-md-12">
                     <div class="tab-content profile-tab" id="myTabContent">
-                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <div class="tab-pane fade show active centrar" id="home" role="tabpanel" aria-labelledby="home-tab">
                             <div class="row">
-                                <div class="col-md-6">
-                                    <label>User Id</label>
+                                <div class="col-md-4">
+                                    <label>usuario</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <p>Kshiti123</p>
+                                <div class="col-md-3">
+                                    <p>JEVORT</p>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-6">
-                                    <label>Name</label>
+                                <div class="col-md-4">
+                                    <label>Nombre</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <p>Kshiti Ghelani</p>
+                                <div class="col-md-3">
+                                    <p>Juan Vergara</p>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-6">
+                                <div class="col-md-4">
                                     <label>Email</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <p>kshitighelani@gmail.com</p>
+                                <div class="col-md-3">
+                                    <p>jevort@gmail.com</p>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-6">
-                                    <label>Phone</label>
+                                <div class="col-md-4">
+                                    <label>Telefono</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <p>123 456 7890</p>
+                                <div class="col-md-3">
+                                    <p>305 2399951</p>
                                 </div>
                             </div>
                             <div class="row">
-                                <div class="col-md-6">
-                                    <label>Profession</label>
+                                <div class="col-md-4">
+                                    <label>Direccion</label>
                                 </div>
-                                <div class="col-md-6">
-                                    <p>Web Developer and Designer</p>
+                                <div class="col-md-3">
+                                    <p>Sincelejo - Sucre</p>
                                 </div>
                             </div>
                         </div>
                         <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Experience</label>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>Expert</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Hourly Rate</label>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>10$/hr</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Total Projects</label>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>230</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>English Level</label>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>Expert</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <label>Availability</label>
-                                </div>
-                                <div class="col-md-6">
-                                    <p>6 months</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <label>Your Bio</label><br />
-                                    <p>Your detail description</p>
-                                </div>
-                            </div>
+
+                            <table class="table">
+                                <thead class="thead-dark">
+                                    <tr>
+                                        <th scope="col">Hotel</th>
+                                        <th scope="col">Ciudad</th>
+                                        <th scope="col">Direccion</th>
+                                        <th scope="col">Fecha Llegada</th>
+                                        <th scope="col">Fecha Salida</th>
+                                        <th scope="col">Valor</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Dan carton</td>
+                                        <td>Bucaramanga</td>
+                                        <td>Cl 53 18 34</td>
+                                        <td>2021/08/15</td>
+                                        <td>2021/08/18</td>
+                                        <td>$ 250000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dan carton</td>
+                                        <td>Bucaramanga</td>
+                                        <td>Cl 53 18 34</td>
+                                        <td>2021/08/15</td>
+                                        <td>2021/08/18</td>
+                                        <td>$ 250000</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Dan carton</td>
+                                        <td>Bucaramanga</td>
+                                        <td>Cl 53 18 34</td>
+                                        <td>2021/08/15</td>
+                                        <td>2021/08/18</td>
+                                        <td>$ 250000</td>
+                                    </tr>
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
