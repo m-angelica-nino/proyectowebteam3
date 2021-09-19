6085de88e52a860089bfe7a87a6d659fd43dee0f
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-18 20:09:01 -0500

    organización JuanElias
    organización...

--- Vista/plantillanav.php
+++ Vista/plantillanav.php
@@ -1,6 +1,6 @@
 
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <li class="navbar-brand" href="#"><strong>Proyecto Web 3</strong></li>
+  <li class="navbar-brand" href="#"><strong>DONDE ME QUEDO</strong></li>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -8,13 +8,19 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Principal <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="../Vista/principal.php">Inicio <span class="sr-only">(current)</span></a>
       </li>
       
       <li class="nav-item">
         <a class="nav-link" href="#">Acerca De</a>
       </li>
-      <!------- forrmato para desabilitar un link
+      <li class="nav-item">
+        <a class="nav-link" href="../Vista/registro.php">Registro</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../Vista/login.php">Login</a>
+      </li>
+      <!------- forrmato para deshabilitar un link
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
@@ -26,14 +32,14 @@
     <li class="nav-item dropdown displey-none">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
            <!-- Perfil <img src="" alt="foto perfil" class="imgPerfil"> -->
-          <img alt="Brand" class="imgperfil" src="../src/imgUsuarios/Juan_Elias.jpeg">
+          <img alt="Brand" class="imgperfil" src="../src/imgUsuarios/Maria_Niño.jpeg">
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="#"><span class="glyphicon glyphicon-user" aria-hidden="true"></span> Mi Perfil</a>
           <a class="dropdown-item" href="#"></span> Mis Reservas</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="#"><span class="glyphiconglyphicon-user"></span> Salir</a>
-        </div>
+
         
       </li>
     <!---- Formato para busqueda
--- sprint2/Vista/registro.php
+++ sprint2/Vista/registro.php
@@ -4,19 +4,23 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
-    <title>Registro</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+    
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" /> -->
 
-    <link rel="stylesheet" href="../css/estilos.css" />
-    <link rel="stylesheet" href="../css/estyloslogin.css" />
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="js/jquery-3.2.1.js"></script>
     <script src="js/script.js"></script>
+
+    <title>Registro</title>
+    <link rel="stylesheet" href="../css/estilos.css" />
+    <link rel="stylesheet" href="../css/estyloslogin.css" />
 </head>
 
 <body>
