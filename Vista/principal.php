5040e9903fd099dd7dbab120e6ca5bdf4ca77712
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-24 18:31:05 -0500

    principal
    ok

--- Vista/principal.php
+++ Vista/principal.php
@@ -278,7 +278,7 @@
       <h5 class="cardA-title">!LAS MEJORES OFERTAS¡¡¡</h5>
       <p class="card-text">Suscríbete y verás los mejores precios.</p>
       <div label for="exampleInputEmail1" class="form-label">email</div>
-      <a href="#" class="btn btn-primary alin">Suscribirme</a>
+      <a href="#" class="btn Sub_btn alin">Suscribirme</a>
     </div>
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
