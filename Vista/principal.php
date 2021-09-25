ecc2cff9e5071e505c2370de2ac1fc44a337cb55
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-25 10:51:29 -0500

    Update principal.php

--- Vista/principal.php
+++ Vista/principal.php
@@ -277,8 +277,8 @@
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="js/jquery-3.2.1.js"></script>
+  
+  <script src="../js/jquery-3.2.1.js"></script>
   <script type="text/javascript" src="https://counter10.stat.ovh/private/counter.js?c=ztnm42u52y2hdcw64prc52g98t48f51g&down=async" async></script>
   <noscript><a href="https://www.contadorvisitasgratis.com" title="contador de visitas html">
 </body>
