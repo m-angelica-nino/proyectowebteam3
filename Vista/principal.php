bc7f10a7e1caf9f1cea24dd619d09c75d17d22c9
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-25 11:34:34 -0500

    Estilos
    estilos a botones y Card Ofertas

--- Vista/principal.php
+++ Vista/principal.php
@@ -21,7 +21,7 @@
     <h1>RESERVA TU HOTEL</h1>
     <section class="">
       <p>
-        <button class="btn btn-light " ; type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <button class="btn btn-dark Sub_btn " ; type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           Filtros +
         </button>
       </p>
@@ -143,7 +143,7 @@
                 <p class="card-text">
                   $235.000
                 </p>
-                <a href="#" class="btn btn-dark alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin Sub_btn">Reservar</a>
               </div>
             </div>
           </div>
@@ -172,7 +172,7 @@
                 <p class="card-text">
                   $210.700
                 </p>
-                <a href="#" class="btn btn-dark alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin Sub_btn">Reservar</a>
               </div>
             </div>
           </div>
@@ -201,7 +201,7 @@
                 <p class="card-text">
                   $202.500
                 </p>
-                <a href="#" class="btn btn-dark alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin Sub_btn">Reservar</a>
               </div>
             </div>
           </div>
@@ -228,7 +228,7 @@
                 <p class="card-text">
                   $175.000
                 </p>
-                <a href="#" class="btn btn-dark alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin Sub_btn">Reservar</a>
               </div>
             </div>
           </div>
@@ -254,7 +254,7 @@
                 <p class="card-text">
                   $135.000
                 </p>
-                <a href="#" class="btn btn-dark alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin Sub_btn">Reservar</a>
               </div>
             </div>
           </div>
@@ -266,7 +266,7 @@
       </section>
     </div>
   </div>
-  <div class="cardB text-white bg-info mb-4 rounded-top" style="max-width: 15rem;">
+  <div class="cardA text-white bg-info mb-4 rounded-top" style="max-width: 15rem;">
     <div class="card-header">Descubre</div>
     <div class="card-body">
       <h5 class="cardB-title"><br><br><br><br> !LAS MEJORES OFERTAS¡¡¡</h5>
@@ -277,8 +277,8 @@
   </div>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  
-  <script src="../js/jquery-3.2.1.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <script src="js/jquery-3.2.1.js"></script>
   <script type="text/javascript" src="https://counter10.stat.ovh/private/counter.js?c=ztnm42u52y2hdcw64prc52g98t48f51g&down=async" async></script>
   <noscript><a href="https://www.contadorvisitasgratis.com" title="contador de visitas html">
 </body>
