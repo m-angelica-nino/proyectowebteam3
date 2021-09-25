e84da07db9bc4c6ff2ab3fc98d49271dffdb7322
m-angelica-nino <90117053+m-angelica-nino@users.noreply.github.com>
2021-09-24 21:42:10 -0500

    dark
    dark

--- Vista/principal.php
+++ Vista/principal.php
@@ -21,7 +21,7 @@
     <h1>RESERVA TU HOTEL</h1>
     <section class="">
       <p>
-        <button class="btn btn-secondary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+        <button class="btn btn-dark" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           Filtros +
         </button>
       </p>
