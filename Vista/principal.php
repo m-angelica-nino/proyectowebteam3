03f75dd2c555fa7e8bbf72047c92db3c6deb3691
JEVORT <89227740+JEVORT@users.noreply.github.com>
2021-09-24 19:12:01 -0500

    Update principal.php

--- Vista/principal.php
+++ Vista/principal.php
@@ -261,14 +261,10 @@
             </div>
           </div>
         </div>
-
-
-
         <div id="sfcztnm42u52y2hdcw64prc52g98t48f51g"></div>
         <img src="https://counter10.stat.ovh/private/contadorvisitasgratis.php?c=ztnm42u52y2hdcw64prc52g98t48f51g" border="0" title="contador de visitas html" alt="contador de visitas html"></a></noscript>
 
         <!-- fin de seccion de carga-->
-
       </section>
     </div>
   </div>
