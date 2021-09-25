dad9ee995c2be28194639e7c97607894e25158fa
m-angelica-nino <90117053+m-angelica-nino@users.noreply.github.com>
2021-09-24 21:55:11 -0500

    dark
    dark

--- Vista/principal.php
+++ Vista/principal.php
@@ -254,7 +254,7 @@
                 <p class="card-text">
                   $135.000
                 </p>
-                <a href="#" class="btn btn-primary alin">Reservar</a>
+                <a href="#" class="btn btn-dark alin">Reservar</a>
               </div>
             </div>
           </div>
