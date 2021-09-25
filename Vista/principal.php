b2b863a1e0a3bda3ff509945d8f21d3ef12c7bdb
m-angelica-nino <90117053+m-angelica-nino@users.noreply.github.com>
2021-09-24 22:10:53 -0500

    img-thumbnail
    img-thumbnail

--- Vista/principal.php
+++ Vista/principal.php
@@ -123,7 +123,7 @@
         <div class="card" style="width: 100%">
           <div class="row no-gutters">
             <div class="col-sm-3">
-              <img class="card-img tam img-thumbnail" style="max-width: 18rem;" src="../src/images/danncarlton.jpeg" alt="300x300" />
+              <img class="card-img tam img-thumbnail"  src="../src/images/danncarlton.jpeg" alt="Suresh Dasari Card" />
             </div>
             <div class="col-sm-5">
               <div class="card-body">
@@ -152,7 +152,7 @@
         <div class="card" style="width: 100%">
           <div class="row no-gutters">
             <div class="col-sm-3">
-              <img class="card-img tam" src="../src/images/tryp.jpg" alt="Suresh Dasari Card" />
+              <img class="card-img tam img-thumbnail" src="../src/images/tryp.jpg" alt="Suresh Dasari Card" />
             </div>
             <div class="col-sm-5">
               <div class="card-body">
